826b8f59b6d919f13ed8be86a332530ccde5a518
Tom Pease <tompease95@gmail.com>
2023-01-11 14:24:31 +0000

    changed main dataloader to create holdout, fixed normalization issues

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,17 +1,21 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
 
 
 def scale(df):
   scaler = MinMaxScaler()
   # I'm not sure why you need the iloc on the right hand side here, but you do...
-  df.iloc[:,0:-1] = scaler.fit_transform(df.iloc[:,0:-1])
+  df.iloc[:,:] = scaler.fit_transform(df.iloc[:,:])
   return df
 
 class AirbnbLoader():
   def __init__(self):
     self.complete_df = pd.read_csv('/Users/tompease/Documents/Coding/airbnb/data/tabular_data/clean_tabular_data.csv')
     self.numeric_df = self.complete_df.select_dtypes(['number'])
+    self.X_test = None
+    self.y_test = None
 
   def load_airbnb(self, label, normalized=True):
     if normalized == True:
@@ -23,8 +27,15 @@ class AirbnbLoader():
     # This is because at this point df is just numeric so if you have the category as the label then it won't be in df
     if label in df.columns:
       features_df = df.drop([label], axis=1)
+      label_df = df.loc[:, label]
     else:
       features_df = df
+      label_df = self.complete_df.loc[:, label]
 
-    label_df = self.complete_df.loc[:, label]
-    return features_df, label_df
+    # This stores a holdout set in the loader which should be the same every time
+    X_train, self.X_test, y_train, self.y_test = train_test_split(features_df, label_df, test_size=0.15, random_state=42)
+    
+    return X_train, y_train
+
+  def load_test_sets(self):
+    return self.X_test, self.y_test
--- utils/pytorch_dataset.py
+++ utils/pytorch_dataset.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 import torch
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from data_loader import AirbnbLoader
 
 def scale(df):
   scaler = MinMaxScaler()
@@ -9,35 +10,26 @@ def scale(df):
   df.iloc[:,0:-1] = scaler.fit_transform(df.iloc[:,0:-1])
   return df
 
-class AirbnbNightlyPriceImageDataset(Dataset):
-  def __init__(self, normalize=True):
-    complete_df = pd.read_csv('/Users/tompease/Documents/Coding/airbnb/data/tabular_data/clean_tabular_data.csv')
-    self.numeric_df = complete_df.select_dtypes(['number'])
-    self.normalize = normalize
+class AirbnbPytorchDataset(Dataset):
+  def __init__(self, label, normalize=True):
+    data = AirbnbLoader()
+    self.X, self.y = data.load_airbnb(label, normalize)
 
   def __getitem__(self, index):
-    if self.normalize == True:
-      df = scale(self.numeric_df)
-      df = pd.DataFrame(df)
-    else:
-      df = self.numeric_df
-    
-    series = df.iloc[index]
     # These have to be cast as 32 bit tensors as the default is 64, but the default weights for pytorch linear layers are float32s, and they have to match...
-    features = torch.tensor(series.drop(['Price_Night']), dtype=torch.float32)
-    label = torch.tensor(series.loc[['Price_Night']], dtype=torch.float32)
+    features = torch.tensor(self.X[index], dtype=torch.float32)
+    label = torch.tensor(self.y[index], dtype=torch.float32)
 
     return (features, label)
 
   def __len__(self):
-    return len(self.numeric_df)
+    return len(self.X)
 
-dataset = AirbnbNightlyPriceImageDataset(normalize=True)
+dataset = AirbnbPytorchDataset('Price_Night', normalize=True)
 
 split_datasets = {}
-split_datasets['train'], split_datasets['val'], split_datasets['test']= random_split(dataset, [0.7, 0.15, 0.15], generator=torch.Generator().manual_seed(42))
+split_datasets['train'], split_datasets['val'] = random_split(dataset, [0.8, 0.2], generator=torch.Generator().manual_seed(42))
 
 data_loaders = {}
 data_loaders['train'] = DataLoader(split_datasets['train'], batch_size=8, shuffle=True)
 data_loaders['val'] = DataLoader(split_datasets['val'], batch_size=8, shuffle=True)
-data_loaders['test'] = DataLoader(split_datasets['test'], batch_size=8, shuffle=True)
